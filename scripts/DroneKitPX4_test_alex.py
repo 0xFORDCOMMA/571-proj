84336741650cec9949413d0387137004c6a4b923
Alex <auman66@gmail.com>
2019-11-23 11:20:39 -0700

    added special world for iris to fly through reef. Also added color/texture to reef model

---
+++ scripts/DroneKitPX4_test_alex.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+################################################################################################
+# @File DroneKitPX4.py
+# Example usage of DroneKit with PX4
+#
+# @author Sander Smeets <sander@droneslab.com>
+#
+# Code partly based on DroneKit (c) Copyright 2015-2016, 3D Robotics.
+################################################################################################
+
+# Import DroneKit-Python
+from dronekit import connect, Command, LocationGlobal
+from pymavlink import mavutil
+from copy import copy
+import time, sys, argparse, math
+
+
+################################################################################################
+# Settings
+################################################################################################
+
+connection_string = '127.0.0.1:14540'
+MAV_MODE_AUTO = 4
+# https://github.com/PX4/Firmware/blob/master/Tools/mavlink_px4.py
+
+
+# Parse connection argument
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--connect", help="connection string")
+args = parser.parse_args()
+
+if args.connect:
+    connection_string = args.connect
+
+
+################################################################################################
+# Init
+################################################################################################
+
+# Connect to the Vehicle
+print "Connecting"
+vehicle = connect(connection_string, wait_ready=True)
+
+def PX4setMode(mavMode):
+    vehicle._master.mav.command_long_send(vehicle._master.target_system, vehicle._master.target_component,
+                                               mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
+                                               mavMode,
+                                               0, 0, 0, 0, 0, 0)
+
+
+
+def get_location_offset_meters(original_location, dNorth, dEast, alt):
+    """
+    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the
+    specified `original_location`. The returned Location adds the entered `alt` value to the altitude of the `original_location`.
+    The function is useful when you want to move the vehicle around specifying locations relative to
+    the current vehicle position.
+    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
+    For more information see:
+    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+    """
+    earth_radius=6378137.0 #Radius of "spherical" earth
+    #Coordinate offsets in radians
+    dLat = dNorth/earth_radius
+    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
+
+    #New position in decimal degrees
+    newlat = original_location.lat + (dLat * 180/math.pi)
+    newlon = original_location.lon + (dLon * 180/math.pi)
+    return LocationGlobal(newlat, newlon,original_location.alt+alt)
+
+
+
+
+
+################################################################################################
+# Listeners
+################################################################################################
+
+home_position_set = False
+
+#Create a message listener for home position fix
+@vehicle.on_message('HOME_POSITION')
+def listener(self, name, home_position):
+    global home_position_set
+    home_position_set = True
+
+
+
+################################################################################################
+# Start mission example
+################################################################################################
+
+#wait for a home position lock
+while not home_position_set:
+    print "Waiting for home position..."
+    time.sleep(1)
+
+print "Home Location Set %s" % vehicle.home_location
+
+# Display basic vehicle state
+print " Type: %s" % vehicle._vehicle_type
+print " Armed: %s" % vehicle.armed
+print " System status: %s" % vehicle.system_status.state
+print " GPS: %s" % vehicle.gps_0
+print " Alt: %s" % vehicle.location.global_relative_frame.alt
+
+# Change to AUTO mode
+PX4setMode(MAV_MODE_AUTO)
+time.sleep(1)
+
+# Load commands
+cmds = vehicle.commands
+cmds.clear()
+
+start = copy(vehicle.location.global_frame)
+print "start location %s" % start
+
+home = vehicle.location.global_relative_frame
+
+#x & y seem to be backwards
+
+#takeoff to starting point
+wp = get_location_offset_meters(home, -3, -7.5, 4.25);
+cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+cmds.add(cmd)
+print "current location %s" % vehicle.location.global_relative_frame
+
+# move to 2nd point
+wp = get_location_offset_meters(home, -3, -4.5, 4.75);
+cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+cmds.add(cmd)
+print "current location %s" % vehicle.location.global_relative_frame
+
+# move to 3rd point
+wp = get_location_offset_meters(home, -3, -1.5, 6.25);
+cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+cmds.add(cmd)
+print "current location %s" % vehicle.location.global_relative_frame
+
+# move to 4th point
+wp = get_location_offset_meters(home, -3, 1.5, 5.75);
+cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+cmds.add(cmd)
+print "current location %s" % vehicle.location.global_relative_frame
+
+# move to 5th point
+wp = get_location_offset_meters(home, -3, 4.5, 4.875);
+cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+cmds.add(cmd)
+print "current location %s" % vehicle.location.global_relative_frame
+
+# move to 6th point
+wp = get_location_offset_meters(home, -3, 7.5, 4.675);
+cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+cmds.add(cmd)
+print "current location %s" % vehicle.location.global_relative_frame
+
+# # land
+# wp = get_location_offset_meters(wp, 0, 0, 10);
+# cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+# cmds.add(cmd)
+
+# # move 10 meters east
+# wp = get_location_offset_meters(start, -1.5, -3, 1.25);
+# cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+# cmds.add(cmd)
+
+# # move 10 meters south
+# wp = get_location_offset_meters(start, 1.5, -3, 0.75);
+# cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+# cmds.add(cmd)
+
+# # move 10 meters west
+# wp = get_location_offset_meters(start, 4.5, -3, -0.125);
+# cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+# cmds.add(cmd)
+
+# land
+wp = get_location_offset_meters(home, 0, 0, 0);
+cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+cmds.add(cmd)
+
+# Upload mission
+cmds.upload()
+time.sleep(2)
+
+# Arm vehicle
+vehicle.armed = True
+
+# monitor mission execution
+nextwaypoint = vehicle.commands.next
+while nextwaypoint < len(vehicle.commands):
+    if vehicle.commands.next > nextwaypoint:
+        display_seq = vehicle.commands.next+1
+        print "Moving to waypoint %s" % display_seq
+        nextwaypoint = vehicle.commands.next
+    time.sleep(1)
+
+# wait for the vehicle to land
+while vehicle.commands.next > 0:
+    time.sleep(1)
+
+
+# Disarm vehicle
+vehicle.armed = False
+time.sleep(1)
+
+# Close vehicle object before exiting script
+vehicle.close()
+time.sleep(1)
