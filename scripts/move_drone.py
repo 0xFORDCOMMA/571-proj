cdaa84b1f2a5a56f161494a9d053551a71f7685e
Maxfield Lehman <_@maxfieldlehman.com>
2019-11-25 11:30:02 -0700

    Update move_drone to use new initial state generation and States.

--- scripts/move_drone.py
+++ scripts/move_drone.py
@@ -16,7 +16,7 @@ import math
 import copy, json, time, os
 from dronekit import connect, Command, LocationGlobal
 from pymavlink import mavutil
-
+import Api_functions as api
 def get_location_offset_meters(original_location, dNorth, dEast, alt):
     """
     Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the
@@ -37,88 +37,78 @@ def get_location_offset_meters(original_location, dNorth, dEast, alt):
     newlon = original_location.lon + (dLon * 180/math.pi)
     return LocationGlobal(newlat, newlon,original_location.alt+alt)
 
+
 def get_offsets(grid, x, y):
     return float(grid[x][y]['x'])+6, float(grid[x][y]['y'])+10, float(grid[x][y]['z'])+5.5
 
-def init_drone(grid):
-    connection_string = '127.0.0.1:14540'
-    MAV_MODE_AUTO = 4
-
-    print "Connecting"
-    vehicle = connect(connection_string, wait_ready=True)
-
-    def PX4setMode(mavMode):
-        vehicle._master.mav.command_long_send(vehicle._master.target_system, vehicle._master.target_component, mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0, mavMode, 0, 0, 0, 0, 0, 0)
-
-    home_position_set = False
-
-    #Create a message listener for home position fix
-    @vehicle.on_message('HOME_POSITION')
-    def listener(self, name, home_position):
-        global home_position_set
-        home_position_set = True
-
-    home_position_set = False
-
-    #wait for a home position lock
-#    while not home_position_set:
-#        print "Waiting for home position..."
-#        time.sleep(1)
-
-    print "Home Location Set %s" % vehicle.home_location
-
-    # Display basic vehicle state
-    print " Type: %s" % vehicle._vehicle_type
-    print " Armed: %s" % vehicle.armed
-    print " System status: %s" % vehicle.system_status.state
-    print " GPS: %s" % vehicle.gps_0
-    print " Alt: %s" % vehicle.location.global_relative_frame.alt
-
-    # Change to AUTO mode
-    PX4setMode(MAV_MODE_AUTO)
-    time.sleep(1)
-
-    # Load commands
-    cmds = vehicle.commands
-    cmds.clear()
-
-
-    home = vehicle.location.global_relative_frame
-
-    #takeoff to starting point
-    wp = get_location_offset_meters(home, 0, 0, 10);
-    cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
-    cmds.add(cmd)
-
-    # TODO: Move to actual starting position
-    wp = get_location_offset_meters(home, *get_offsets(grid, 0, 0))
-    cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
-    cmds.add(cmd)
-
-    # Upload mission
-    cmds.upload()
-    time.sleep(2)
-
-    # Arm vehicle
-    vehicle.armed = True
-
-    # monitor mission execution
-    nextwaypoint = vehicle.commands.next
-    while nextwaypoint < len(vehicle.commands):
-        if vehicle.commands.next > nextwaypoint:
-            display_seq = vehicle.commands.next+1
-            print "Moving to waypoint %s" % display_seq
-            nextwaypoint = vehicle.commands.next
-        time.sleep(1)
-
-    return vehicle, home
-
 
 class moveDrone:
+    def init_drone(self):
+        connection_string = '127.0.0.1:14540'
+        MAV_MODE_AUTO = 4
+
+        print "Connecting"
+        self.vehicle = connect(connection_string, wait_ready=True)
+
+        def PX4setMode(mavMode):
+            self.vehicle._master.mav.command_long_send(self.vehicle._master.target_system, self.vehicle._master.target_component, mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0, mavMode, 0, 0, 0, 0, 0, 0)
+
+        #Create a message listener for self.home position fix
+        @self.vehicle.on_message('HOME_POSITION')
+        def listener(self, name, self.home_position):
+            global self.home_position_set
+            self.home_position_set = True
+
+        print "Home Location Set %s" % self.vehicle.home_location
+
+        # Display basic self.vehicle state
+        print " Type: %s" % self.vehicle._self.vehicle_type
+        print " Armed: %s" % self.vehicle.armed
+        print " System status: %s" % self.vehicle.system_status.state
+        print " GPS: %s" % self.vehicle.gps_0
+        print " Alt: %s" % self.vehicle.location.global_relative_frame.alt
+
+        # Change to AUTO mode
+        PX4setMode(MAV_MODE_AUTO)
+        time.sleep(1)
+
+        # Load commands
+        cmds = self.vehicle.commands
+        cmds.clear()
+
+        self.home = self.vehicle.location.global_relative_frame
+
+        #takeoff to starting point
+        wp = get_location_offset_meters(self.home, 0, 0, 10);
+        cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+        cmds.add(cmd)
+
+        wp = get_location_offset_meters(self.home, *get_offsets(self.grid, self.loc.x, self.loc.y))
+        cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
+        cmds.add(cmd)
+
+        # Upload mission
+        cmds.upload()
+        time.sleep(2)
+
+        # Arm self.vehicle
+        self.vehicle.armed = True
+
+        # monitor mission execution
+        nextwaypoint = self.vehicle.commands.next
+        while nextwaypoint < len(self.vehicle.commands):
+            if self.vehicle.commands.next > nextwaypoint:
+                display_seq = self.vehicle.commands.next+1
+                print "Moving to waypoint %s" % display_seq
+                nextwaypoint = self.vehicle.commands.next
+            time.sleep(1)
+
+
     def __init__(self):
         rospy.init_node('move_drone',anonymous = True)
         self.actions = String()
-        self.loc = (0,0,"north")
+        self.helper = api.Helper()
+        self.loc = self.helper.get_initial_state()
         self.action_subscriber = rospy.Subscriber('/actions',String,self.callback_actions)
         self.status_publisher = rospy.Publisher("/status",String,queue_size = 10)
         self.free = String(data = "next")
@@ -126,7 +116,7 @@ class moveDrone:
         self.root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
         with open(self.root_path + '/reef.json', 'r') as f:
             self.grid = json.load(f)
-        self.vehicle, self.home = init_drone(self.grid)
+        self.init_drone()
         print("Ready!")
         rospy.spin()
 
@@ -137,24 +127,21 @@ class moveDrone:
 
     def execute_next(self):
         action = self.actions.pop(0)
-        direction = None
         if action == "MoveF" or action == "MoveB":
             current_loc = self.loc
-            orientation = current_loc[2]            
 
             deltas = {'MoveF': {'north': (0,1),  'south': (0,-1), 'east': (1,0),  'west': (-1,0)},
                       'MoveB': {'north': (0,-1), 'south': (0,1),  'east': (-1,0), 'west': (1,0)}  }
-            new_loc = tuple([sum(dim) for dim in zip(current_loc[:2], deltas[action][orientation])] + [orientation])
-            x, y = new_loc[0], new_loc[1]
+            new_loc = api.State(*([sum(dim) for dim in zip(current_loc[:2], deltas[action][self.loc.orientation])] + [self.loc.orientation]))
 
             # Load commands
             cmds = self.vehicle.commands
             #cmds.clear()
 
-            if 0 <= x < 6 and 0 <= y < 4:
+            if 0 <= new_loc.x < 6 and 0 <= new_loc.y < 4:
                 print "Taking action %s to go from %s to %s\n" % (action, current_loc, new_loc)
                 self.loc = new_loc
-                wp = get_location_offset_meters(self.home, *get_offsets(self.grid, x, y))
+                wp = get_location_offset_meters(self.home, *get_offsets(self.grid, self.loc.x, self.loc.y))
                 cmd = Command(0,0,0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 1, 0, 0, 0, 0, wp.lat, wp.lon, wp.alt)
                 cmds.add(cmd)
 
@@ -164,15 +151,14 @@ class moveDrone:
                 print "Attempted to move out of grid.\n"
 
         elif action == "TurnCW" or action == "TurnCCW":
-            x, y = self.loc[0], self.loc[1]
             res_orientations = {'north': {'TurnCW': 'east', 'TurnCCW': 'west'},
                                 'south': {'TurnCW': 'west', 'TurnCCW': 'east'},
                                 'east':  {'TurnCW': 'south','TurnCCW': 'north'},
                                 'west':  {'TurnCW': 'north','TurnCCW': 'south'}}
-            new_loc = (x, y, res_orientations[self.loc[2]][action])
-            print "Taking action %s to go from %s to %s\n" % (action, self.loc, new_loc)
+            new_loc = api.State(self.loc.x, self.loc.y, res_orientations[self.loc.orientation][action])
+            print "Taking action %s to go from %s to %s" % (action, self.loc, new_loc)
             self.loc = new_loc
-            wp = get_location_offset_meters(self.home, *get_offsets(self.grid, x, y))
+            wp = get_location_offset_meters(self.home, *get_offsets(self.grid, self.loc.x, self.loc.y))
             Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_CONDITION_YAW, 0, 1, 90, 0, 1 if action == "TurnCW" else -1, 1, wp.lat, wp.lon, wp.alt)
 
         else:
