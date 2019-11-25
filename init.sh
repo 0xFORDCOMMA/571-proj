d5ae94913fe8e10ad63047f5705a98a8e2d4d74b
Maxfield Lehman <_@maxfieldlehman.com>
2019-11-25 14:38:00 -0700

    Add shebang to VM init script.

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > \
     /etc/apt/sources.list.d/ros-latest.list && \
     apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116 && \
